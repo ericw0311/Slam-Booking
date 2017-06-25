b70d59d13e6095dc045466636272c8d64a893a85
ericw0311 <eric.willard@slam-data.net>
2017-06-25 07:00:06 +0200

    20170625_am1

--- src/SD/CoreBundle/Form/QueryBookingType.php
+++ src/SD/CoreBundle/Form/QueryBookingType.php
@@ -17,15 +17,15 @@ class QueryBookingType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder->add('name', TextType::class, array('label' => 'queryBooking.name', 'translation_domain' => 'messages'))
-
-
 			->add('periodType', ChoiceType::class, array(
 				'label' => 'period.type',
 				'translation_domain' => 'messages',
 	'choices' => array('NO' => 'NO', 'BETWEEN' => 'BETWEEN', 'AFTER' => 'AFTER', 'BEFORE' => 'BEFORE', 'SYSDATE' => 'SYSDATE', 'AFTER.SYSDATE' => 'AFTER.SYSDATE', 'BEFORE.SYSDATE' => 'BEFORE.SYSDATE'),
 	'choice_label' => function ($value, $key, $index) { return 'period.type.'.$key; }
         ))
-			->add('beginningDate', DateType::class, array('label' => 'from', 'translation_domain' => 'messages', 
+			->add('beginningDate', DateType::class, array('label' => 'from', 'translation_domain' => 'messages',
+			'widget' => 'single_text', 'html5' => false, 'format' => 'dd/MM/yyyy', 'attr' => ['class' => 'datepicker']))
+			->add('endDate', DateType::class, array('label' => 'to', 'translation_domain' => 'messages',
 			'widget' => 'single_text', 'html5' => false, 'format' => 'dd/MM/yyyy', 'attr' => ['class' => 'datepicker']));
     }
     
---
+++ src/SD/CoreBundle/Resources/public/js/queryBooking.js
@@ -0,0 +1,22 @@
+var periodType = document.getElementById('sd_corebundle_queryBooking_periodType');
+var beginningDate = document.getElementById('sd_corebundle_queryBooking_beginningDate');
+var endDate = document.getElementById('sd_corebundle_queryBooking_endDate');
+
+periodType.addEventListener('change', function() {
+	var periodTypeValue = periodType.options[periodType.selectedIndex].value;
+	// alert(beginningDate.value);
+	// alert(periodTypeValue);
+	if (periodTypeValue == "BETWEEN" || periodTypeValue == "AFTER") {
+		beginningDate.disabled = false;
+	} else {
+		beginningDate.value = "";
+		beginningDate.disabled = true;
+	}
+
+	if (periodTypeValue == "BETWEEN" || periodTypeValue == "BEFORE") {
+		endDate.disabled = false;
+	} else {
+		endDate.value = "";
+		endDate.disabled = true;
+	}
+});

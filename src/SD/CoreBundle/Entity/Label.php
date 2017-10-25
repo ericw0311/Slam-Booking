ef9f681a9c2432e749dad63f942b091e84df02d0
ericw0311 <eric.willard@slam-data.net>
2017-10-25 06:27:43 +0200

    20171025_am1

---
+++ src/SD/CoreBundle/Entity/Label.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace SD\CoreBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Label
+ *
+ * @ORM\Table(name="label")
+ * @ORM\Entity(repositoryClass="SD\CoreBundle\Repository\LabelRepository")
+ */
+class Label
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="name", type="string", length=255)
+     */
+    private $name;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     *
+     * @return Label
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+}
+

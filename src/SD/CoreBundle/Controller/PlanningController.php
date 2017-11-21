b4553eddff68349f2db4f80b2748fedc6acca857
ericw0311 <eric.willard@slam-data.net>
2017-11-21 22:38:27 +0100

    20171121_pm1

--- src/SD/CoreBundle/Controller/PlanningController.php
+++ src/SD/CoreBundle/Controller/PlanningController.php
@@ -34,14 +34,24 @@ class PlanningController extends Controller
     }
 
 	// Acces au planning d'une planification
-	return $this->redirectToRoute('sd_core_planning_calendar', array('planificationID' => $firstPlanification->getID(), 'date' => date("Y-m-d")));
+	return $this->redirectToRoute('sd_core_planning_calendar', array('planificationID' => $firstPlanification->getID(), 'date' => date("Ymd")));
+	}
+
+
+    public function noplanificationAction()
+    {
+    $connectedUser = $this->getUser();
+    $em = $this->getDoctrine()->getManager();
+    $userContext = new UserContext($em, $connectedUser); // contexte utilisateur
+
+    return $this->render('SDCoreBundle:Planning:noplanification.html.twig', array('userContext' => $userContext));
 	}
 
 
     // Affichage du calendrier d'une planification
     /**
     * @ParamConverter("planification", options={"mapping": {"planificationID": "id"}})
-	* @ParamConverter("date", options={"format": "Y-m-d"})
+	* @ParamConverter("date", options={"format": "Ymd"})
     */
     public function calendarAction(Planification $planification, \Datetime $date)
     {
@@ -61,7 +71,7 @@ class PlanningController extends Controller
     // Affichage de la grille horaire journaliere d'une planification
     /**
 	* @ParamConverter("planification", options={"mapping": {"planificationID": "id"}})
-	* @ParamConverter("date", options={"format": "Y-m-d"})
+	* @ParamConverter("date", options={"format": "Ymd"})
 	*/
     public function timetableAction(Planification $planification, \Datetime $date)
     {
@@ -73,7 +83,12 @@ class PlanningController extends Controller
 
     $listPlanifications = $planificationRepository->getPlanifications($userContext->getCurrentFile());
 
+	$previousDate = clone $date;
+	$previousDate->sub(new \DateInterval('P1D'));
+	$nextDate = clone $date;
+	$nextDate->add(new \DateInterval('P1D'));
+
     return $this->render('SDCoreBundle:Planning:timetable.html.twig',
-		array('userContext' => $userContext, 'listPlanifications' => $listPlanifications, 'planificationID' => $planification->getID(), 'date' => $date));
+		array('userContext' => $userContext, 'listPlanifications' => $listPlanifications, 'planificationID' => $planification->getID(), 'date' => $date, 'nextDate' => $nextDate, 'previousDate' => $previousDate));
     }
 }

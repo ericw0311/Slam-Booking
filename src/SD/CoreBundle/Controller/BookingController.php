e9673a66871cb5de60cbc398aef9e1166785e5c4
ericw0311 <eric.willard@slam-data.net>
2017-12-12 06:22:18 +0100

    20171212_am1

--- src/SD/CoreBundle/Controller/BookingController.php
+++ src/SD/CoreBundle/Controller/BookingController.php
@@ -18,21 +18,61 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
 
 class BookingController extends Controller
 {
-    // Affichage de la grille horaire journaliere d'une planification
+    // Création de réservation
     /**
 	* @ParamConverter("planification", options={"mapping": {"planificationID": "id"}})
     * @ParamConverter("planificationPeriod", options={"mapping": {"planificationPeriodID": "id"}})
     * @ParamConverter("resource", options={"mapping": {"resourceID": "id"}})
 	* @ParamConverter("date", options={"format": "Ymd"})
 	*/
-    public function createAction(Planification $planification, PlanificationPeriod $planificationPeriod, Resource $resource, \Datetime $date, $cells)
+    public function many_createAction(Planification $planification, PlanificationPeriod $planificationPeriod, Resource $resource, $cells)
+    {
+	return BookingController::create_Action($planification, $planificationPeriod, $resource, $cells, 1);
+    }
+
+    // Création de réservation
+    /**
+	* @ParamConverter("planification", options={"mapping": {"planificationID": "id"}})
+    * @ParamConverter("planificationPeriod", options={"mapping": {"planificationPeriodID": "id"}})
+    * @ParamConverter("resource", options={"mapping": {"resourceID": "id"}})
+	* @ParamConverter("date", options={"format": "Ymd"})
+	*/
+    public function one_createAction(Planification $planification, PlanificationPeriod $planificationPeriod, Resource $resource, $cells)
+    {
+	return BookingController::create_Action($planification, $planificationPeriod, $resource, $cells, 0);
+    }
+
+    // Création de réservation
+    public function create_Action(Planification $planification, PlanificationPeriod $planificationPeriod, Resource $resource, $cells, $many)
     {
     $connectedUser = $this->getUser();
     $em = $this->getDoctrine()->getManager();
     $userContext = new UserContext($em, $connectedUser); // contexte utilisateur
 
-    return $this->render('SDCoreBundle:Booking:create.html.twig',
-		array('userContext' => $userContext, 'planification' => $planification, 'planificationPeriod' => $planificationPeriod,
-			'resource' => $resource, 'cells' => $cells, 'date' => $date));
+	$cellArray  = explode("+", $cells);
+
+    $ttlRepository = $em->getRepository('SDCoreBundle:TimetableLine');
+
+	list($beginningDateString, $beginningTimetableID, $beginningTimetableLineID) = explode("-", $cellArray[0]);
+	$beginningDate = date_create_from_format("Ymd", $beginningDateString);
+
+	$beginningTimetableLine = $ttlRepository->find($beginningTimetableLineID);
+
+	list($endDateString, $endTimetableID, $endTimetableLineID) = explode("-", $cellArray[count($cellArray)-1]);
+	$endDate = date_create_from_format("Ymd", $endDateString);
+
+	$endTimetableLine = $ttlRepository->find($endTimetableLineID);
+
+	if ($many) {
+		return $this->render('SDCoreBundle:Booking:create.many.html.twig',
+array('userContext' => $userContext, 'planification' => $planification, 'planificationPeriod' => $planificationPeriod, 'resource' => $resource,
+	'beginningDate' => $beginningDate, 'beginningTimetableLine' => $beginningTimetableLine,
+	'endDate' => $endDate, 'endTimetableLine' => $endTimetableLine));
+	} else {
+		return $this->render('SDCoreBundle:Booking:create.one.html.twig',
+array('userContext' => $userContext, 'planification' => $planification, 'planificationPeriod' => $planificationPeriod, 'resource' => $resource,
+	'beginningDate' => $beginningDate, 'beginningTimetableLine' => $beginningTimetableLine,
+	'endDate' => $endDate, 'endTimetableLine' => $endTimetableLine));
+	}
     }
 }

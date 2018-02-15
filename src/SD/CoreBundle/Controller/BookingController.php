73cc6334d8b0612bcbf4425ecf0c10cccb91cc6b
ericw0311 <eric.willard@slam-data.net>
2018-02-15 23:18:05 +0100

    20180215_pm1

--- src/SD/CoreBundle/Controller/BookingController.php
+++ src/SD/CoreBundle/Controller/BookingController.php
@@ -404,9 +404,6 @@ array('userContext' => $userContext, 'booking' => $booking, 'planification' => $
 	'selectedUserFiles' => $selectedUserFiles, 'availableUserFiles' => $availableUserFiles, 'userFileIDList' => $userFileIDList, 'userFileIDInitialList' => $userFileIDInitialList));
     }
 
-
-
-
 	// Validation de la création d'une réservation
     /**
 	* @ParamConverter("planification", options={"mapping": {"planificationID": "id"}})
@@ -418,7 +415,6 @@ array('userContext' => $userContext, 'booking' => $booking, 'planification' => $
 	return BookingController::validate_createAction($planification, $planificationPeriod, $resource, $timetableLinesList, $userFileIDList, $request, 1);
     }
 
-
     // Validation de la création d'une réservation
     /**
 	* @ParamConverter("planification", options={"mapping": {"planificationID": "id"}})
@@ -429,7 +425,6 @@ array('userContext' => $userContext, 'booking' => $booking, 'planification' => $
     {
 	return BookingController::validate_createAction($planification, $planificationPeriod, $resource, $timetableLinesList, $userFileIDList, $request, 0);
     }
-
 
 	// Validation de la création d'une réservation
     public function validate_createAction(Planification $planification, PlanificationPeriod $planificationPeriod, Resource $resource, $timetableLinesList, $userFileIDList, Request $request, $many)
@@ -496,6 +491,122 @@ array('userContext' => $userContext, 'booking' => $booking, 'planification' => $
 	$em->flush();
 	$request->getSession()->getFlashBag()->add('notice', 'booking.created.ok');
 
+	return $this->redirectToRoute('sd_core_planning_'.($many ? 'many' : 'one').'_timetable',
+		array('planificationID' => $planification->getID(), 'planificationPeriodID' => $planificationPeriod->getID(), 'date' => $beginningDateString));
+    }
+
+	// Validation de la mise à jour d'une réservation
+    /**
+	* @ParamConverter("booking", options={"mapping": {"bookingID": "id"}})
+	* @ParamConverter("planification", options={"mapping": {"planificationID": "id"}})
+    * @ParamConverter("planificationPeriod", options={"mapping": {"planificationPeriodID": "id"}})
+    * @ParamConverter("resource", options={"mapping": {"resourceID": "id"}})
+	*/
+	public function many_validate_updateAction(Booking $booking, Planification $planification, PlanificationPeriod $planificationPeriod, Resource $resource, $timetableLinesList, $userFileIDList, Request $request)
+	{
+	return BookingController::validate_updateAction($booking, $planification, $planificationPeriod, $resource, $timetableLinesList, $userFileIDList, $request, 1);
+	}
+
+	// Validation de la mise à jour d'une réservation
+    /**
+	* @ParamConverter("booking", options={"mapping": {"bookingID": "id"}})
+	* @ParamConverter("planification", options={"mapping": {"planificationID": "id"}})
+    * @ParamConverter("planificationPeriod", options={"mapping": {"planificationPeriodID": "id"}})
+    * @ParamConverter("resource", options={"mapping": {"resourceID": "id"}})
+	*/
+	public function one_validate_updateAction(Booking $booking, Planification $planification, PlanificationPeriod $planificationPeriod, Resource $resource, $timetableLinesList, $userFileIDList, Request $request)
+    {
+	return BookingController::validate_updateAction($booking, $planification, $planificationPeriod, $resource, $timetableLinesList, $userFileIDList, $request, 0);
+    }
+
+	// Validation de la création d'une réservation
+    public function validate_updateAction(Booking $booking, Planification $planification, PlanificationPeriod $planificationPeriod, Resource $resource, $timetableLinesList, $userFileIDList, Request $request, $many)
+    {
+	$connectedUser = $this->getUser();
+	$em = $this->getDoctrine()->getManager();
+	$userContext = new UserContext($em, $connectedUser); // contexte utilisateur
+	$plRepository = $em->getRepository('SDCoreBundle:PlanificationLine');
+    $tRepository = $em->getRepository('SDCoreBundle:Timetable');
+    $tlRepository = $em->getRepository('SDCoreBundle:TimetableLine');
+    $ufRepository = $em->getRepository('SDCoreBundle:UserFile');
+    $blRepository = $em->getRepository('SDCoreBundle:BookingLine');
+    $buRepository = $em->getRepository('SDCoreBundle:BookingUser');
+	
+	$urlArray  = explode("-", $timetableLinesList);
+	
+	// Mise à jour des dates heures mini et maxi de la réservation.
+	list($beginningDateString, $beginningTimetableID, $beginningTimetableLinesList) = explode("+", $urlArray[0]);
+	$beginningTimetableLines = explode("*", $beginningTimetableLinesList);
+	$beginningTimetableLineID = $beginningTimetableLines[0];
+	$beginningTimetableLine = $tlRepository->find($beginningTimetableLineID);
+	$booking->setBeginningDate(date_create_from_format('YmdHi', $beginningDateString.$beginningTimetableLine->getBeginningTime()->format('Hi')));
+	list($endDateString, $endTimetableID, $endTimetableLinesList) = explode("+", $urlArray[count($urlArray)-1]);
+	$endTimetableLines = explode("*", $endTimetableLinesList);
+	$endTimetableLineID = $endTimetableLines[count($endTimetableLines)-1];
+	$endTimetableLine = $tlRepository->find($endTimetableLineID);
+	$booking->setEndDate(date_create_from_format('YmdHi', $endDateString.$endTimetableLine->getEndTime()->format('Hi')));
+	$em->persist($booking);
+
+	$url_timetableLinesString = BookingApi::getTimetableLines($timetableLinesList);
+	
+	// Parcours des lignes de réservation dans l'ordre chronologique.
+	$bookingLines = $blRepository->findBy(array('booking' => $booking), array('date' => 'asc', 'timetable' => 'asc', 'timetableLine' => 'asc')); // TPRR. Voir le champ date (date ou ddate)
+	
+	foreach ($bookingLines as $bookingLine) {
+		$booking_timetableLineString = $bookingLine->getDate()->format('Ymd').'-'.$bookingLine->getTimetable()->getID().'-'.$bookingLine->getTimetableLine()->getID();
+		
+		if (!in_array($booking_timetableLineString, $url_timetableLinesString)) { // La ligne de réservation n'appartient pas aux lignes de l'Url. Elle est supprimée.
+			$em->remove($bookingLine);
+		}
+	}
+
+	// Parcours des lignes de réservation de l'Url.
+	foreach ($url_timetableLinesString as $url_timetableLineString) {
+
+		list($dateString, $timetableID, $timetableLineID) = explode("-", $url_timetableLineString);
+		$date = date_create_from_format('Ymd', $dateString);
+		// Recherche de la ligne de réservation en base.
+		$bookingLineDB = $blRepository->findOneBy(array('resource' => $resource, 'date' => $date, 'timetable' => $tRepository->find($timetableID), 'timetableLine' => $tlRepository->find($timetableLineID)));
+
+		if ($bookingLineDB === null) { // La ligne de réservation n'existe pas en base, on la crée.
+			$bookingLine = new BookingLine($connectedUser, $booking, $resource);
+			$bookingLine->setDate($date);
+			$bookingLine->setPlanification($planification);
+			$bookingLine->setPlanificationPeriod($planificationPeriod);
+			$bookingLine->setPlanificationLine($plRepository->findOneBy(array('planificationPeriod' => $planificationPeriod, 'weekDay' => strtoupper($date->format('D')))));
+			$bookingLine->setTimetable($tRepository->find($timetableID));
+			$bookingLine->setTimetableLine($tlRepository->find($timetableLineID));
+			$em->persist($bookingLine);
+		}
+	}
+	
+	// Tableau des utilisateurs de l'Url
+	$url_userFileID = explode("-", $userFileIDList);
+
+	// Parcours des utilisateurs de la réservation.
+	$bookingUsers = $buRepository->findBy(array('booking' => $booking), array('id' => 'asc'));
+	
+	// Parcours des utilisateurs de la reservation.
+	foreach ($bookingUsers as $bookingUser) {
+		if (!in_array($bookingUser->getUserFile()->getID(), $url_userFileID)) { // L'utilisateur n'appartient pas a la liste de l'Url. Il est supprimé.
+			$em->remove($bookingUser);
+		}
+	}
+
+	$order = 0;
+	// Parcours des utilisateurs de l'Url.
+	foreach ($url_userFileID as $userFileID) {
+		$bookingUser = $buRepository->findOneBy(array('booking' => $booking, 'userFile' => $ufRepository->find($userFileID)));
+
+		if ($bookingUser === null) { // L'utilisateur n'est pas rattaché en base à la réservation --> on crée le lien.
+			$bookingUser = new BookingUser($connectedUser, $booking, $ufRepository->find($userFileID));
+		}
+		$bookingUser->setOrder(++$order); // Pour tous les utilisateurs de l'Url, on met à jour le numéro d'ordre
+		$em->persist($bookingUser);
+	}
+
+	$em->flush();
+	$request->getSession()->getFlashBag()->add('notice', 'booking.updated.ok');
 	return $this->redirectToRoute('sd_core_planning_'.($many ? 'many' : 'one').'_timetable',
 		array('planificationID' => $planification->getID(), 'planificationPeriodID' => $planificationPeriod->getID(), 'date' => $beginningDateString));
     }

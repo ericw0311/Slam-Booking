8d3c0441b10a306ff9382b6bedf80c61a8f768f1
ericw0311 <eric.willard@slam-data.net>
2018-03-07 23:14:30 +0100

    20180307_pm1

--- src/SD/CoreBundle/Resources/public/js/calendar-settings.js
+++ src/SD/CoreBundle/Resources/public/js/calendar-settings.js
@@ -9,15 +9,29 @@ $(function () {
         header: {
             left: 'prev, next',
             center: 'title',
-            right: 'month, agendaWeek, agendaDay, listWeek'
+            right: 'month, agendaWeek, agendaDay'
         },
         lazyFetching: true,
+        buttonText: {
+            month: 'mois',
+            week: 'semaine',
+            day: 'jour'
+        },
+        firstDay: 1,
+		dayNames: ['Dimanche','Lundi','Mardi','Mercredi','Jeudi','Vendredi','Samedi'],
+		dayNamesShort: ['Dim','Lun','Mar','Mer','Jeu','Ven','Sam'],
+		monthNames: ['Janvier','Février','Mars','Avril','Mai','Juin','Juillet','Aout','Septembre','Octobre','Novembre','Décembre'],
+		monthNamesShort: ['Jan','Fév','Mar','Avr','Mai','Jun','Jul','Aou','Sep','Oct','Nov','Déc'],
         timeFormat: {
             // for agendaWeek and agendaDay
             agenda: 'h:mmt',    // 5:00 - 6:30
-
             // for all other views
             '': 'h:mmt'         // 7p
+        },
+        titleFormat: {
+            month: 'MMMM yyyy',
+            week: 'MMMM yyyy',
+            day: 'dddd d MMMM yyyy'
         },
         eventSources: [
             {

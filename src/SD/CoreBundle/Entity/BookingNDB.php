afc8850f6281d18ee21013419c07dab6f53c874f
ericw0311 <eric.willard@slam-data.net>
2018-06-22 00:02:44 +0200

    20180621_pm1

--- src/SD/CoreBundle/Entity/BookingNDB.php
+++ src/SD/CoreBundle/Entity/BookingNDB.php
@@ -10,9 +10,9 @@ class BookingNDB
 	private $type;
 	private $numberTimetableLines;
 	private $cellClass;
-	private $userFiles;
 	private $userNamesString;
-	private $labels;
+	private $labelNamesString;
+	private $note;
 
 	public function setId($id)
 	{
@@ -58,28 +58,6 @@ class BookingNDB
 	return $this->cellClass;
 	}
 
-	public function setUserFiles($userFiles)
-	{
-	$this->userFiles = $userFiles;
-	return $this;
-	}
-
-	public function getUserFiles()
-	{
-	return $this->userFiles;
-	}
-
-	public function setLabels($labels)
-	{
-	$this->labels = $labels;
-	return $this;
-	}
-
-	public function getLabels()
-	{
-	return $this->labels;
-	}
-
 	public function setUserNamesString($userNamesString)
 	{
 	$this->userNamesString = $userNamesString;
@@ -91,14 +69,51 @@ class BookingNDB
 	return $this->userNamesString;
 	}
 	
+	public function setLabelNamesString($labelNamesString)
+	{
+	$this->labelNamesString = $labelNamesString;
+	return $this;
+	}
+
+	public function getLabelNamesString()
+	{
+	return $this->labelNamesString;
+	}
+	
+	public function getLabel() // Indique si au moins une étiquette a été attachée à la réservation
+	{
+	return (!empty($this->labelNamesString));
+	}
+
+	public function setNote($note)
+	{
+	$this->note = $note;
+	return $this;
+	}
+
+	public function getNote()
+	{
+	return $this->note;
+	}
+	
 	public function __construct($id, $type, $cellClass)
 	{
 	$this->setId($id);
 	$this->setType($type);
 	$this->setNumberTimetableLines(0);
 	$this->setCellClass($cellClass);
-	$this->setUserFiles(null);
-	$this->setLabels(null);
 	$this->setUserNamesString(null);
+	$this->setLabelNamesString(null);
+	$this->setNote(null);
+	}
+
+	public function getNoteExists()
+	{
+	return (!empty($this->note));
+	}
+	
+	public function getMultiLigne() // Une réservation est affichée en multiligne si elle est multi créneaux horaires et qu'elle a une ou plusieurs étiquettes
+	{
+	return ($this->getNumberTimetableLines() > 1 && $this->getLabel());
 	}
 }
